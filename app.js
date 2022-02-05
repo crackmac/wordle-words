671ac85bbeac3ce78e6a486a8c48e5eddd0e5106
Kevin Duane <kevin.duane@disney.com>
2022-02-05 15:06:38 -0800

    added framework

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
-const express = require("express");
+"use strict";
 
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 8000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Worlde!");
+// Static files
+app.use(express.static("public"));
+// Specific folder example
+// app.use('/css', express.static(__dirname + 'public/css'))
+// app.use('/js', express.static(__dirname + 'public/js'))
+// app.use('/img', express.static(__dirname + 'public/images'))
+
+// Set Views
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get("", (req, res) => {
+  res.render("index", { text: "Wordle Words" });
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(__dirname + "/views/about.html");
 });
 
 app.listen(port, () => {
