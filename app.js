9f68f1da6ee854422a13872396f91c7b3962b871
Kevin Duane <kevin.duane@disney.com>
2022-02-08 23:14:06 -0800

    added word filtering

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-"use strict";
+'use strict';
 
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 8000;
 
 // Static files
-app.use(express.static("public"));
+app.use(express.static('public'));
 // Specific folder example
 // app.use("/css", express.static(__dirname + "public/css"));
 // app.use("/js", express.static(__dirname + "public/js"));
 // app.use("/img", express.static(__dirname + "public/images"));
 
 // Set Views
-app.set("views", "./views");
-app.set("view engine", "ejs");
+app.set('views', './views');
+app.set('view engine', 'ejs');
 
-app.get("", (req, res) => {
-  res.render("index", { text: "Wordle Words" });
+app.get('/', (req, res) => {
+  res.render('index', { text: 'Wordle Words' });
 });
 
-app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
-});
+// app.get("/about", (req, res) => {
+//   res.sendFile(__dirname + "/views/about.html");
+// });
 
 app.listen(port, () => {
-  console.log("App is running");
+  console.log('App is running');
 });
--- public/js/script.js
+++ public/js/script.js
@@ -17,12 +17,85 @@ const find_words_input3 = document.querySelector('.form_find_words_input3');
 const find_words_input4 = document.querySelector('.form_find_words_input4');
 const find_words_input5 = document.querySelector('.form_find_words_input5');
 
+// Functions
+function randomizeArray(arr) {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+function wordSearch(letters) {
+  let words = [];
+
+  wordlist.filter(w => {
+    let count = letters.length;
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      if (w.includes(letter)) {
+        count--;
+        if (count === 0) {
+          // console.log(`Word is ${w}`);
+          words.push(w);
+        }
+      }
+    }
+  });
+  if (words.length === 0) {
+    console.error('words.length is zero');
+    words.push('Sorry, no results. Try again.');
+  }
+  // console.log(words);
+  return words;
+}
+
+function wordSearch2(letters) {
+  let words = [];
+
+  console.log(`Letters: ${letters}`);
+  console.log(`wordlist.lenght: ${wordlist.length}`);
+
+  wordlist.filter(w => {
+    // console.log(w);
+    let count = letters.length;
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      if (letter === undefined) {
+        continue;
+      }
+      if (letter.toUpperCase() === letter && w[i] === letter.toLowerCase()) {
+        // console.log(`We have a match! ${w[i]}, ${letter.toLowerCase()}, ${w}`);
+        count--;
+        // console.log(`Count is ${count}`);
+        if (count === 0) {
+          // console.log(`Word is ${w}`);
+          words.push(w);
+        }
+      } else if (w.includes(letter)) {
+        count--;
+        if (count === 0) {
+          // console.log(`Word is ${w}`);
+          words.push(w);
+        }
+      }
+    }
+  });
+  if (words.length === 0) {
+    console.error('words.length is zero');
+    words.push('Sorry, no results. Try again.');
+  }
+  // console.log(words);
+  return words;
+}
+
+// Button functions
 starter_btn.addEventListener('click', () => {
   starterResults1.textContent = '';
   starterResults2.textContent = '';
   // find words from short list of popular letters
   const sliceBestLetters = randomizeArray(bestLetters).slice(3);
-  const mixedWords = randomizeArray(wordsWithLetters(sliceBestLetters));
+  const mixedWords = randomizeArray(wordSearch2(sliceBestLetters));
   // console.log(
   //   `Five words containing the most common letters ${sliceBestLetters}\n${mixedWords.slice(
   //     0,
@@ -43,42 +116,12 @@ find_words_btn.addEventListener('click', () => {
   guess[3] = find_words_input4.value;
   guess[4] = find_words_input5.value;
   // find words from supplied letters
-  // const find_words_results = randomizeArray(
-  //   wordsWithLetters(['a', 'l', 't', 'o'])
-  // );
-  // console.log(`${guess}`);
-  find_wordsResults1.textContent = `${guess}`;
-  find_wordsResults2.textContent = `${guess}`;
+
+  const mixedWords = randomizeArray(wordSearch2(guess));
+
+  console.log(`${guess}`);
+  console.log(`${mixedWords}`);
+  find_wordsResults1.textContent = `Some words containing your supplied letters ${guess}`;
+  find_wordsResults2.textContent = `${mixedWords.slice(0, 4)}`;
+  // find_wordsResults2.textContent = `${guess}`;
 });
-
-function randomizeArray(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
-}
-
-function wordsWithLetters(letters) {
-  let words = [];
-
-  wordlist.filter(w => {
-    let count = letters.length;
-    for (let i = 0; i < letters.length; i++) {
-      const letter = letters[i];
-      if (w.includes(letter)) {
-        count--;
-        if (count === 0) {
-          // console.log(`Word is ${w}`);
-          words.push(w);
-        }
-      }
-    }
-  });
-  if (words.length === 0) {
-    console.error('words.length is zero');
-    words.push('Sorry, no results. Try again.');
-  }
-  console.log(words);
-  return words;
-}
